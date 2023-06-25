ac6814ef50481466ad6e6aad29c79ed5191ed651
yotamos5699 <m.lhafakot@gmail.com>
2023-06-25 20:08:07 +0300

    asd

--- dist/app.js
+++ dist/app.js
@@ -21,15 +21,9 @@ const fs_1 = __importDefault(require("fs"));
 const f_path = "./" + "lastqr" + ".json";
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
-const writeFileIfExist = (data) => {
-    if (fs_1.default.existsSync(f_path))
-        fs_1.default.writeFileSync(f_path, JSON.stringify(data), {
-            encoding: "utf8",
-            flag: "w",
-        });
-    else
-        fs_1.default.writeFileSync(f_path, JSON.stringify(data), { encoding: "utf8" });
-};
+app.use((0, cors_1.default)({
+    origin: "*",
+}));
 const client = new whatsapp_web_js_1.Client({
     authStrategy: new whatsapp_web_js_1.LocalAuth(),
     puppeteer: {
@@ -38,24 +32,33 @@ const client = new whatsapp_web_js_1.Client({
     },
 });
 app.set("trust proxy", 1);
-client.on("qr", (qr) => {
-    qrcode_terminal_1.default.generate(qr, { small: true }, (qrcode) => {
-        console.log(qrcode);
-        const time = new Date().getTime();
-        writeFileIfExist({ time: time, qr: qrcode });
+app.post("/api/getQr", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("getQr");
+    const state = yield client.getState();
+    console.log({ state });
+    if (state)
+        return res.send({ status: true, data: "client connected" });
+    return client.on("qr", (qr) => {
+        qrcode_terminal_1.default.generate(qr, { small: true }, (qrcode) => {
+            console.log(qrcode);
+            if (qr)
+                return res.send({ status: false, data: qr });
+            else
+                return res.send({ status: true, data: "client connected" });
+        });
     });
-});
+}));
+app.post("/api/userState", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("getQr");
+    const state = yield client.getState();
+    return res.send({ status: state ? true : false, data: state });
+}));
 client.on("ready", () => {
     console.log("Client is ready!");
-    const time = new Date().getTime();
-    writeFileIfExist({ time: time, qr: "ready" });
 });
 app.get("/", (_req, res) => {
     res.send("Hello World!");
 });
-app.use((0, cors_1.default)({
-    origin: "*",
-}));
 app.use(express_1.default.json());
 app.listen(PORT, () => console.log(`server? listening on port` + PORT));
 app.get("/api/qr", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -83,109 +86,57 @@ const sendToMennagers = (actionLog, msg) => {
     mennagersNumbers.forEach((number) => client.sendMessage(number, Message));
 };
 app.post("/api/sendMsgs", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _c;
     console.log("send sms api");
     let { numbers, msg } = yield req.body;
     let actionLog = [];
     let record;
     let isArrey = Array.isArray(msg);
+    const state = yield client.getState();
+    if (!state)
+        return res.send({ status: false, data: "not connected" });
+    console.log({ state });
     if (isArrey && msg.length != numbers.length)
         return res.send({ status: "no", data: "msg arrey != numbers arrey" });
-    const x = client;
-    if ((_c = x === null || x === void 0 ? void 0 : x.pupBrowser) === null || _c === void 0 ? void 0 : _c._connection) {
-        for (let i = 0; i <= numbers.length - 1; i++) {
-            let log = ``;
-            let Message = `${isArrey ? msg[i] : msg}`;
-            try {
-                console.log("number: ", numbers[i], " message: ", msg[i]);
-                yield client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered) {
-                    if (isRegistered) {
-                        console.log("is registerd !!");
-                        client.sendMessage(`${numbers[i]}@c.us`, Message);
-                        record = {
-                            number: numbers[i],
-                            status: "ok",
-                            row: i,
-                            msg: Message,
-                        };
-                    }
-                    else {
-                        console.log("is not registerd !!");
-                        log = `***** ${numbers[i]} is not registerd ******`;
-                        record = {
-                            number: numbers[i],
-                            status: "registretion error",
-                            row: i,
-                            msg: log,
-                        };
-                    }
-                });
-            }
-            catch (err) {
-                record = {
-                    number: numbers[i],
-                    status: "catch error",
-                    row: i,
-                    msg: err,
-                };
-            }
-            actionLog.push(record);
-        }
-        sendToMennagers(actionLog);
-        return res.send(actionLog);
-    }
-    else {
-        client
-            .initialize()
-            .then((res) => __awaiter(void 0, void 0, void 0, function* () {
-            console.log({ res });
-            console.log("client initialize ....\n to init in initializ");
-            for (let i = 0; i <= numbers.length - 1; i++) {
-                let log = ``;
-                let Message = `${isArrey ? msg[i] : msg}`;
-                try {
-                    yield client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered) {
-                        if (isRegistered) {
-                            client.sendMessage(`${numbers[i]}@c.us`, Message);
-                            record = {
-                                number: numbers[i],
-                                status: "ok",
-                                row: i,
-                                msg: Message,
-                            };
-                        }
-                        else {
-                            log = `***** ${numbers[i]} is not registerd ******`;
-                            record = {
-                                number: numbers[i],
-                                status: "registretion error",
-                                row: i,
-                                msg: log,
-                            };
-                        }
-                    });
-                }
-                catch (err) {
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        let log = ``;
+        let Message = `${isArrey ? msg[i] : msg}`;
+        try {
+            console.log("number: ", numbers[i], " message: ", msg[i]);
+            yield client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered) {
+                if (isRegistered) {
+                    console.log("is registerd !!");
+                    client.sendMessage(`${numbers[i]}@c.us`, Message);
                     record = {
                         number: numbers[i],
-                        status: "catch error",
+                        status: "ok",
                         row: i,
-                        msg: err,
+                        msg: Message,
                     };
                 }
-                actionLog.push(record);
-            }
-        }))
-            .then(() => {
-            sendToMennagers(actionLog);
-            return res.send(actionLog);
-        })
-            .catch((e) => {
-            sendToMennagers(actionLog, "error server not working");
-            return res.send(e);
-        });
+                else {
+                    console.log("is not registerd !!");
+                    log = `***** ${numbers[i]} is not registerd ******`;
+                    record = {
+                        number: numbers[i],
+                        status: "registretion error",
+                        row: i,
+                        msg: log,
+                    };
+                }
+            });
+        }
+        catch (err) {
+            record = {
+                number: numbers[i],
+                status: "catch error",
+                row: i,
+                msg: err,
+            };
+        }
+        actionLog.push(record);
     }
-    console.log(actionLog);
+    sendToMennagers(actionLog);
+    return res.send(actionLog);
 }));
 const serviceName = "972545940054@c.us";
 client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* () {
--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,19 @@ const f_path = "./" + "lastqr" + ".json";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const writeFileIfExist = (data: any) => {
-  if (fs.existsSync(f_path))
-    fs.writeFileSync(f_path, JSON.stringify(data), {
-      encoding: "utf8",
-      flag: "w",
-    });
-  else fs.writeFileSync(f_path, JSON.stringify(data), { encoding: "utf8" });
-};
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// const writeFileIfExist = (data: any) => {
+//   if (fs.existsSync(f_path))
+//     fs.writeFileSync(f_path, JSON.stringify(data), {
+//       encoding: "utf8",
+//       flag: "w",
+//     });
+//   else fs.writeFileSync(f_path, JSON.stringify(data), { encoding: "utf8" });
+// };
 
 //let toSend: boolean = true;
 const client = new Client({
@@ -29,30 +33,41 @@ const client = new Client({
 });
 
 app.set("trust proxy", 1); // trust first proxy
-client.on("qr", (qr: any) => {
-  qrcode.generate(qr, { small: true }, (qrcode) => {
-    console.log(qrcode);
-    const time = new Date().getTime();
-    writeFileIfExist({ time: time, qr: qrcode });
+
+app.post("/api/getQr", async (_req: express.Request, res: express.Response) => {
+  console.log("getQr");
+  const state = await client.getState();
+  console.log({ state });
+  if (state) return res.send({ status: true, data: "client connected" });
+  return client.on("qr", (qr: any) => {
+    qrcode.generate(qr, { small: true }, (qrcode) => {
+      console.log(qrcode);
+      if (qr) return res.send({ status: false, data: qr });
+      else return res.send({ status: true, data: "client connected" });
+      // const time = new Date().getTime();
+      // writeFileIfExist({ time: time, qr: qrcode });
+    });
   });
 });
+app.post("/api/userState", async (_req: express.Request, res: express.Response) => {
+  console.log("getQr");
+  const state = await client.getState();
+  return res.send({ status: state ? true : false, data: state });
+});
+// client.on("", (message) => console.log({ message }));
 
 client.on("ready", () => {
   // toSend = true;
   console.log("Client is ready!");
-  const time = new Date().getTime();
-  writeFileIfExist({ time: time, qr: "ready" });
+  // const time = new Date().getTime();
+  // writeFileIfExist({ time: time, qr: "ready" });
 });
 //npm i --save-dev @types/node
 // ******************************** SERVER INIT **************************************//
 app.get("/", (_req: any, res: { send: (arg0: string) => void }): void => {
   res.send("Hello World!");
 });
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+
 app.use(express.json());
 app.listen(PORT, () => console.log(`server? listening on port` + PORT));
 //******************************  main sript  *************************************/
@@ -103,101 +118,50 @@ app.post(
     let record: any;
     let isArrey: boolean = Array.isArray(msg);
 
+    const state = await client.getState();
+    if (!state) return res.send({ status: false, data: "not connected" });
+    console.log({ state });
+
     if (isArrey && msg.length != numbers.length) return res.send({ status: "no", data: "msg arrey != numbers arrey" });
-    const x: any = client;
-    // console.log("conenction stat on top", x?.pupBrowser?._connection, client.pupBrowser?.isConnected);
-    if (x?.pupBrowser?._connection) {
-      // console.log("conenction stat on _conection == true", x?.pupBrowser?._connection, client.pupBrowser?.isConnected);
-      for (let i = 0; i <= numbers.length - 1; i++) {
-        let log = ``;
-        let Message = `${isArrey ? msg[i] : msg}`;
-        try {
-          console.log("number: ", numbers[i], " message: ", msg[i]);
-          await client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered: any) {
-            if (isRegistered) {
-              console.log("is registerd !!");
-              client.sendMessage(`${numbers[i]}@c.us`, Message);
-              record = {
-                number: numbers[i],
-                status: "ok",
-                row: i,
-                msg: Message,
-              };
-            } else {
-              console.log("is not registerd !!");
-              log = `***** ${numbers[i]} is not registerd ******`;
-              record = {
-                number: numbers[i],
-                status: "registretion error",
-                row: i,
-                msg: log,
-              };
-            }
-          });
-        } catch (err) {
-          record = {
-            number: numbers[i],
-            status: "catch error",
-            row: i,
-            msg: err,
-          };
-        }
-        actionLog.push(record);
-      }
-      sendToMennagers(actionLog);
-      return res.send(actionLog);
-    } else {
-      client
-        .initialize()
-        .then(async (res) => {
-          console.log({ res });
 
-          console.log("client initialize ....\n to init in initializ");
-
-          for (let i = 0; i <= numbers.length - 1; i++) {
-            let log = ``;
-            let Message = `${isArrey ? msg[i] : msg}`;
-            try {
-              await client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered: any) {
-                if (isRegistered) {
-                  client.sendMessage(`${numbers[i]}@c.us`, Message);
-                  record = {
-                    number: numbers[i],
-                    status: "ok",
-                    row: i,
-                    msg: Message,
-                  };
-                } else {
-                  log = `***** ${numbers[i]} is not registerd ******`;
-                  record = {
-                    number: numbers[i],
-                    status: "registretion error",
-                    row: i,
-                    msg: log,
-                  };
-                }
-              });
-            } catch (err) {
-              record = {
-                number: numbers[i],
-                status: "catch error",
-                row: i,
-                msg: err,
-              };
-            }
-            actionLog.push(record);
+    for (let i = 0; i <= numbers.length - 1; i++) {
+      let log = ``;
+      let Message = `${isArrey ? msg[i] : msg}`;
+      try {
+        console.log("number: ", numbers[i], " message: ", msg[i]);
+        await client.isRegisteredUser(`${numbers[i]}@c.us`).then(function (isRegistered: any) {
+          if (isRegistered) {
+            console.log("is registerd !!");
+            client.sendMessage(`${numbers[i]}@c.us`, Message);
+            record = {
+              number: numbers[i],
+              status: "ok",
+              row: i,
+              msg: Message,
+            };
+          } else {
+            console.log("is not registerd !!");
+            log = `***** ${numbers[i]} is not registerd ******`;
+            record = {
+              number: numbers[i],
+              status: "registretion error",
+              row: i,
+              msg: log,
+            };
           }
-        })
-        .then(() => {
-          sendToMennagers(actionLog);
-          return res.send(actionLog);
-        })
-        .catch((e) => {
-          sendToMennagers(actionLog, "error server not working");
-          return res.send(e);
         });
+      } catch (err) {
+        record = {
+          number: numbers[i],
+          status: "catch error",
+          row: i,
+          msg: err,
+        };
+      }
+      actionLog.push(record);
     }
-    console.log(actionLog);
+    sendToMennagers(actionLog);
+    return res.send(actionLog);
 
     //console.log("conenction stat on end == true", x?.pupBrowser?._connection, client.pupBrowser?.isConnected);
   }
